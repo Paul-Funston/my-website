2952837d2d45190e4ee7e85344a55856a759a050
Paul Funston <paulfunston9@gmail.com>
2022-11-10 10:56:37 -0600

    header js

--- assets/script/index.js
+++ assets/script/index.js
@@ -6,3 +6,36 @@
     My website
 */
 
+// utility functions
+function onEvent(event, selector, callback) {
+  return selector.addEventListener(event, callback);
+}
+
+function select(selector, parent = document) {
+  return parent.querySelector(selector);
+}
+
+function selectAll(selector, parent = document) {
+  return [...parent.querySelectorAll(selector)];
+}
+
+
+// Open and close mobile nav
+const openNav = select('.header-open-nav');
+const closeNav = select('.header-close-nav');
+const nav = select('nav');
+
+onEvent('click', openNav, () => {
+  nav.style.display = 'flex';
+  openNav.classList.add('hidden');
+  closeNav.classList.remove('hidden');
+})
+
+onEvent('click', closeNav, () => {
+  nav.style.display = 'none';
+  openNav.classList.remove('hidden');
+  closeNav.classList.add('hidden');
+})
+
+
+
